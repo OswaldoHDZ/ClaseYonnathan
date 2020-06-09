35d18ba83fc6c55e1d85d800201a18ca37c02860
OswaldoHDZ <oswaldo.hernandez.juarezr@gmail.com>
2020-06-08 20:30:38 -0500

    Hola

---
+++ src/main/java/rmEjemplo/RMIEjemplo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rmEjemplo;
+
+import com.mycompany.servidorinterface.servidorInterface;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ *
+ * @author Oswaldo
+ */
+public class RMIEjemplo {
+    public static void main(String[] args) throws RemoteException{
+        Remote remote = UnicastRemoteObject.exportObject(new servidorInterface(
+                
+        
+        ) {
+            @Override
+            public String holaJava(String nombre) throws RemoteException {
+                return "Hola" + nombre;
+            }
+        },9001);
+        
+    }
+}
